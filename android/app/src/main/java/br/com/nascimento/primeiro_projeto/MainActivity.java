1041629b1adca04c6aa8bca918d0ebd2864fc2b6
xEdsonx <edson.nikicaro@gmail.com>
2019-08-21 20:03:11 -0300

    criando projeto

---
+++ android/app/src/main/java/br/com/nascimento/primeiro_projeto/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.nascimento.primeiro_projeto;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
